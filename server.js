77e30c4c4fbfcda36532f70b2fec1a87c0d3f51d
Glitch (hello-express) <none>
2020-12-01 15:02:42 +0000

    🚛🍇 Checkpoint ./server.js:577339/1333 ./package.json:577339/1282

--- server.js
+++ server.js
@@ -1,34 +1,22 @@
-// server.js
-// where your node app starts
-
-// we've started you off with Express (https://expressjs.com/)
-// but feel free to use whatever libraries or frameworks you'd like through `package.json`.
 const express = require("express");
 const app = express();
+const axios = require('axios')
 
-// our default array of dreams
-const dreams = [
-  "Find and count some sheep",
-  "Climb a really tall mountain",
-  "Wash the dishes"
-];
-
-// make all the files in 'public' available
-// https://expressjs.com/en/starter/static-files.html
-app.use(express.static("public"));
-
-// https://expressjs.com/en/starter/basic-routing.html
-app.get("/", (request, response) => {
-  response.sendFile(__dirname + "/views/index.html");
-});
-
-// send the default array of dreams to the webpage
-app.get("/dreams", (request, response) => {
-  // express helps us take JS objects and send them as JSON
-  response.json(dreams);
-});
-
-// listen for requests :)
+app.use(express.json());
+app.post('/github', async (req, res, next) => {
+  try {
+    const ignored = (reason) => {
+      res.send({ ok: true, ignored: reason })
+    }
+    if (req.body.action !== 'requested') {
+      return ignored('Irrelevant action')
+    }
+    res.send({ ok: true })
+  } catch (error) {
+    console.error(error)
+    res.status(500).send({ ok: false })
+  }
+})
 const listener = app.listen(process.env.PORT, () => {
   console.log("Your app is listening on port " + listener.address().port);
 });
